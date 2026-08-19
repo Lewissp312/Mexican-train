9295b116478a5ba67ee7e65b80fee698d000d486
Lewis Spencer <lewissp@live.co.uk>
2026-08-19 17:50:32 +0100

    Refactoring of train code and allowing dominoes on all trains
    - Slightly changed the spacing of the domino trains to make them clearer
    - Refactored train and gameManager code to be more in line with where they will be in the final game
    - All trains can now generate a domino path

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,43 +7,44 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-    List<int[]> _bestDominoPath;
+    // List<int[]> _bestDominoPath;
     List<int[]> _drawPile;
-    List<int[]> _spareDominoes;
-    Dictionary<int[],GameObject> _dominoObjects;
-    Train _trainScript;
-    [SerializeField] GameObject _train;
+    // List<int[]> _spareDominoes;
+    // Dictionary<int[],GameObject> _dominoObjects;
+    // Train _trainScript;
     [SerializeField] GameObject _domino;
     [SerializeField] GameObject[] _numbers;
+    [SerializeField] GameObject[] trains;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Awake()
     {
         _drawPile = new();
-        _bestDominoPath = new();
-        _spareDominoes = new();
-        _dominoObjects = new();
+        GenerateDrawPile();
     } 
     void Start()
     {
-        _trainScript = _train.GetComponent<Train>();
-        GenerateDrawPile();
-        GeneratePlayerDominoes();
-        FindBestPath();
-        print("Player dominoes");
-        for(int i=0; i < 15; i++)
+        foreach(GameObject train in trains)
         {
-            print($"{_spareDominoes[i][0]},{_spareDominoes[i][1]}");
+            train.GetComponent<Train>().GeneratePlayerDominoes(_drawPile,_numbers);
         }
-        print("Best domino path");
-        foreach(int[] domino in _bestDominoPath)
-        {
-            print($"{domino[0]},{domino[1]}");
-            _spareDominoes.Remove(domino);
-            _trainScript.AddDominoToTrain(_dominoObjects[domino],domino);
-            _dominoObjects.Remove(domino);
-        }
-        print($"Spare domino total: {_spareDominoes.Count}");
+
+        // _trainScript = _train.GetComponent<Train>();
+        // print("Player dominoes");
+        // for(int i=0; i < 15; i++)
+        // {
+        //     print($"{_spareDominoes[i][0]},{_spareDominoes[i][1]}");
+        // }
+        // print("Best domino path");
+        // foreach(int[] domino in _bestDominoPath)
+        // {
+        //     print($"{domino[0]},{domino[1]}");
+        //     _spareDominoes.Remove(domino);
+        //     _dominoObjects[domino].SetActive(true);
+        //     _trainScript.AddDominoToTrain(_dominoObjects[domino],domino);
+        //     _dominoObjects.Remove(domino);
+        // }
+        // print($"Spare domino total: {_spareDominoes.Count}");
     }
 
     // Update is called once per frame
@@ -61,99 +62,6 @@ public class GameManager : MonoBehaviour
                 _drawPile.Add(numCombination);
             }
         }
-    }
-
-    void GeneratePlayerDominoes()
-    {
-        for(int i=0; i < 15; i++)
-        {
-            int[] randDomino = _drawPile[Random.Range(0,_drawPile.Count)];
-            _drawPile.Remove(randDomino);
-            _spareDominoes.Add(randDomino);
-            GameObject dominoCopy = Instantiate(_domino);
-            GameObject firstNumber = Instantiate(_numbers[randDomino[0]],dominoCopy.transform);
-            GameObject secondNumber = Instantiate(_numbers[randDomino[1]],dominoCopy.transform);
-            firstNumber.transform.localPosition = new(0,0.25f,0);
-            secondNumber.transform.localPosition = new(0,-0.25f,0);
-            _dominoObjects.Add(randDomino,dominoCopy);
-        }
-    }
-
-    void FindBestPath()
-    {
-        if(_bestDominoPath.Count > 0)
-        {
-            foreach(int[] domino in _bestDominoPath)
-            {
-                _spareDominoes.Add(domino);
-            }
-            _bestDominoPath.Clear();
-        }
-        List<int[]> currentDominoPath = new();
-        for (int i = 0; i < _spareDominoes.Count; i++)
-        {
-            FindBestPathRecurLoop(currentDominoPath,_trainScript.GetLastPlayedDominoNum());
-        }
-    }
-
-    void FindBestPathRecurLoop(List<int[]> currentDominoPath, int numTomatch)
-    {
-        for (int i = 0; i < _spareDominoes.Count; i++)
-        {
-            if (_spareDominoes[i][0] == numTomatch || _spareDominoes[i][1] == numTomatch)
-            {
-                if (!currentDominoPath.Contains(_spareDominoes[i]))
-                {
-                    currentDominoPath.Add(_spareDominoes[i]);
-                    int otherNum = _spareDominoes[i][0] == numTomatch ? _spareDominoes[i][1] : _spareDominoes[i][0];
-                    FindBestPathRecurLoop(currentDominoPath,otherNum);
-                    currentDominoPath.Remove(_spareDominoes[i]);   
-                }
-            }
-        }
-        if (currentDominoPath.Count > _bestDominoPath.Count)
-        {
-            _bestDominoPath.Clear();
-            foreach(int[] domino in currentDominoPath)
-            {
-                _bestDominoPath.Add(domino);
-            }
-        } else if(currentDominoPath.Count == _bestDominoPath.Count)
-        {
-            if (IsCurrentPathWorthMoreThanBest(currentDominoPath))
-            {
-                _bestDominoPath.Clear();
-                foreach(int[] domino in currentDominoPath)
-                {
-                    _bestDominoPath.Add(domino);
-                }
-            }
-        }
-    }
-
-    bool IsCurrentPathWorthMoreThanBest(List<int[]> currentDominoPath)
-    {
-        int currentDominoPathTotal = 0;
-        int bestDominoPathTotal = 0;
-        for(int i = 0; i < _bestDominoPath.Count; i++)
-        {
-            if (currentDominoPath[i][0] == 0 && currentDominoPath[i][1] == 0)
-            {
-                currentDominoPathTotal += 50;
-            }
-            else
-            {
-                currentDominoPathTotal += currentDominoPath[i][0] + currentDominoPath[i][1];
-            }
-            if (_bestDominoPath[i][0] == 0 && _bestDominoPath[i][1] == 0)
-            {
-                bestDominoPathTotal += 50;
-            }
-            else
-            {
-                bestDominoPathTotal += _bestDominoPath[i][0] + _bestDominoPath[i][1];
-            }
-        }
-        return currentDominoPathTotal > bestDominoPathTotal;
+        print($"Draw pile total: {_drawPile.Count}");
     }
 }
--- Assets/Scripts/Train.cs
+++ Assets/Scripts/Train.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
 public class Train : MonoBehaviour
@@ -5,17 +7,24 @@ public class Train : MonoBehaviour
     int trainSize;
     int _lastPlayedDominoNum;
     Vector3 _lastPlayedDominoPos;
-    Vector3 _rightPosVector = new(0.42f,-0.5f,0);
-    Vector3 _leftPosVector = new(-0.42f,-0.5f,0);
+    Vector3 _rightPosVector = new(0.42f,-0.6f,0);
+    Vector3 _leftPosVector = new(-0.42f,-0.6f,0);
+    Dictionary<int[],GameObject> _dominoObjects;
+    List<int[]> _bestDominoPath;
+    List<int[]> _spareDominoes;
+    [SerializeField] GameObject _domino;
+
     void Awake()
     {
-        _lastPlayedDominoNum = Random.Range(0,13);
+        _lastPlayedDominoNum = 12;
+        _bestDominoPath = new();
+        _spareDominoes = new();
+        _dominoObjects = new();
     } 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
         
     }
 
@@ -25,9 +34,113 @@ public class Train : MonoBehaviour
         
     }
 
-    public void AddDominoToTrain(GameObject domino, int[] dominoNums)
+    public void GeneratePlayerDominoes(List<int[]> drawPile,GameObject[] numbers)
+    { //add the starting num here if you want to control it in game manager (currently always set to the double twelve)
+      //TODO: Adjust the number of dominoes the player gets based on how many players there are
+        do
+        {
+            int[] randDomino = drawPile[Random.Range(0,drawPile.Count)];
+            if (!_spareDominoes.Contains(randDomino))
+            {
+                print("Hello");
+                GameObject dominoCopy = Instantiate(_domino);
+                GameObject firstNumber = Instantiate(numbers[randDomino[0]],dominoCopy.transform);
+                GameObject secondNumber = Instantiate(numbers[randDomino[1]],dominoCopy.transform);
+                firstNumber.transform.localPosition = new(0,0.25f,0);
+                secondNumber.transform.localPosition = new(0,-0.25f,0);
+                dominoCopy.SetActive(false);
+                _dominoObjects.Add(randDomino,dominoCopy);
+                _spareDominoes.Add(randDomino);                
+            }
+        } while (_spareDominoes.Count < 11);
+        foreach(int[] domino in _spareDominoes){drawPile.Remove(domino);}
+        FindBestPath();
+        print("Found best path");
+        foreach(int[] domino in _bestDominoPath){AddDominoToTrain(domino);}
+        // _bestDominoPath.Clear();
+    }
+
+    void FindBestPath()
     {
-        Transform dominoTransform = domino.transform;
+        if(_bestDominoPath.Count > 0)
+        {
+            foreach(int[] domino in _bestDominoPath)
+            {
+                _spareDominoes.Add(domino);
+            }
+            _bestDominoPath.Clear();
+        }
+        List<int[]> currentDominoPath = new();
+        for (int i = 0; i < _spareDominoes.Count; i++)
+        {
+            FindBestPathRecurLoop(currentDominoPath,_lastPlayedDominoNum);
+        }
+    }
+
+    void FindBestPathRecurLoop(List<int[]> currentDominoPath, int numTomatch)
+    {
+        for (int i = 0; i < _spareDominoes.Count; i++)
+        {
+            if (_spareDominoes[i][0] == numTomatch || _spareDominoes[i][1] == numTomatch)
+            {
+                if (!currentDominoPath.Contains(_spareDominoes[i]))
+                {
+                    currentDominoPath.Add(_spareDominoes[i]);
+                    int otherNum = _spareDominoes[i][0] == numTomatch ? _spareDominoes[i][1] : _spareDominoes[i][0];
+                    FindBestPathRecurLoop(currentDominoPath,otherNum);
+                    currentDominoPath.Remove(_spareDominoes[i]);   
+                }
+            }
+        }
+        if (currentDominoPath.Count > _bestDominoPath.Count)
+        {
+            _bestDominoPath.Clear();
+            foreach(int[] domino in currentDominoPath)
+            {
+                _bestDominoPath.Add(domino);
+            }
+        } else if(currentDominoPath.Count == _bestDominoPath.Count)
+        {
+            if (IsCurrentPathWorthMoreThanBest(currentDominoPath))
+            {
+                _bestDominoPath.Clear();
+                foreach(int[] domino in currentDominoPath)
+                {
+                    _bestDominoPath.Add(domino);
+                }
+            }
+        }
+    }
+
+    bool IsCurrentPathWorthMoreThanBest(List<int[]> currentDominoPath)
+    {
+        int currentDominoPathTotal = 0;
+        int bestDominoPathTotal = 0;
+        for(int i = 0; i < _bestDominoPath.Count; i++)
+        {
+            if (currentDominoPath[i][0] == 0 && currentDominoPath[i][1] == 0)
+            {
+                currentDominoPathTotal += 50;
+            }
+            else
+            {
+                currentDominoPathTotal += currentDominoPath[i][0] + currentDominoPath[i][1];
+            }
+            if (_bestDominoPath[i][0] == 0 && _bestDominoPath[i][1] == 0)
+            {
+                bestDominoPathTotal += 50;
+            }
+            else
+            {
+                bestDominoPathTotal += _bestDominoPath[i][0] + _bestDominoPath[i][1];
+            }
+        }
+        return currentDominoPathTotal > bestDominoPathTotal;
+    }
+
+    public void AddDominoToTrain(int[] dominoNums)
+    {
+        Transform dominoTransform = _dominoObjects[dominoNums].transform;
         dominoTransform.parent = transform;
         dominoTransform.rotation = transform.rotation;
         dominoTransform.position = transform.position;
@@ -48,7 +161,9 @@ public class Train : MonoBehaviour
         trainSize++;
         _lastPlayedDominoPos = dominoTransform.localPosition;
         _lastPlayedDominoNum = dominoNums[0] == _lastPlayedDominoNum ? dominoNums[1] : dominoNums[0];
-        //get lastplayeddominonum here
+        // _bestDominoPath.Remove(dominoNums);
+         _dominoObjects[dominoNums].SetActive(true);
+        _dominoObjects.Remove(dominoNums);
     }
 
     public int GetLastPlayedDominoNum()
